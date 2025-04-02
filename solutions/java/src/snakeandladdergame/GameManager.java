c1d1cd8b74306d0f7abafd86f559190a26e43f07
KRN <35370516+KRN05@users.noreply.github.com>
2025-04-02 17:04:03 +0530

    Update GameManager.java

--- solutions/java/src/snakeandladdergame/GameManager.java
+++ solutions/java/src/snakeandladdergame/GameManager.java
@@ -24,3 +24,33 @@ public class GameManager {
         new Thread(() -> game.play()).start();
     }
 }
+
+
+/**
+
+thread part can be also written as 
+
+
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+class GameManager {
+    private List<SnakeAndLadderGame> games = new ArrayList<>();
+    private ExecutorService gameExecutor = Executors.newCachedThreadPool();
+
+    public void startNewGame(List<String> playerNames) {
+        SnakeAndLadderGame game = new SnakeAndLadderGame(playerNames);
+        games.add(game);
+        gameExecutor.submit(game::play); // Uses a thread pool instead of creating new threads
+    }
+}
+
+
+
+*/
+
+
+
+
+
